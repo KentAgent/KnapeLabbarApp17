4a3c56f6a18d53f8b86b6811160b643ec629a497
kristofferknape <Kristoffer.knape@gmail.com>
2018-01-21 13:39:10 +0100

    Initial Commit

---
+++ KnapeLabbarApp17/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnapeLabbarApp17
+//
+//  Created by Kristoffer Knape on 2018-01-21.
+//  Copyright © 2018 Kristoffer Knape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KnapeLabbarApp17/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KnapeLabbarApp17
+//
+//  Created by Kristoffer Knape on 2018-01-21.
+//  Copyright © 2018 Kristoffer Knape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
